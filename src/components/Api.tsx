aa4ff8af8f0056d33f8cabd68f01cc091176b2ec
Onyinye Oguocha <stephanieoguocha@gmail.com>
2025-03-07 10:05:52 +0100

    update IP data retrieval logic to use external API with API key

--- src/components/Api.tsx
+++ src/components/Api.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-// const API_KEY = import.meta.env.VITE_IPIFY_API_KEY;
-// const BASE_URL = "https://geo.ipify.org/api/v2/country,city";
+const API_KEY = import.meta.env.VITE_IPIFY_API_KEY;
+const BASE_URL = "https://geo.ipify.org/api/v2/country,city";
 
 export const getIPData = async (ip: string) => {
   try {
-    const response = await axios.get(`/api/ip?ip=${ip}`);
+    const response = await axios.get(
+      `${BASE_URL}?apiKey=${API_KEY}&ipAddress=${ip}`
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching IP data:", error);
